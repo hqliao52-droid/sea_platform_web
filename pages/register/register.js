54a09a4791ed3f8da0adffe66c4a32d31ec9ee21
hqliao52-droid <hqliao52@gmail.com>
2026-04-21 18:48:57 +0800

    ``` feat(register): 添加用户注册页面的头像上传功能
    - 新增头像上传、选择和删除功能，包括前端界面和后端交互逻辑
    - 扩展注册表单字段，添加昵称和电话输入项
    - 实现头像上传到服务器的功能，支持图片选择和压缩
    - 优化注册页面布局和样式，提升用户体验
    - 修改注册接口调用，新增头像、昵称和电话参数传递
    ```

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,4 +1,5 @@
 import {request} from '@/utils/request.js';
+import icon from 'uview-ui/libs/config/props/icon';
 
 export default {
   data() {
@@ -6,12 +7,65 @@ export default {
       form: {
         account: '',
         password: '',
-        confirmPwd: ''
+        confirmPwd: '',
+        nickName: '',
+        phone: '',
+
       },
+      avatarUrl: '',
       loading: false
     }
   },
   methods: {
+    // 选择头像
+    chooseAvatar() {
+      uni.chooseImage({
+        count: 1,
+        sizeType: ['compressed'],
+        extension: ['jpg', 'jpeg', 'png'], 
+        success: (res) => {
+          const tempFilePath = res.tempFilePaths[0]
+          // 直接上传，不做后缀判断！
+          this.uploadAvatar(tempFilePath)
+        }
+      })
+    },
+    // 上传头像
+    uploadAvatar(filePath) {
+      uni.showLoading({ title: '上传中...' })
+
+      // 【重点】上传文件 不要手动加 header！！！
+      uni.uploadFile({
+        url: 'http://127.0.0.1:8000/file/upload_file',
+        filePath: filePath,
+        name: 'file',
+        method: 'POST',
+        success: (uploadRes) => {
+          try {
+            const data = JSON.parse(uploadRes.data)
+            if (data.code === 200 || data.code === '200') {
+              this.avatarUrl = data.data.url
+              uni.showToast({ title: "上传成功", icon: 'success' })
+            } else {
+              uni.showToast({ title: data.msg || '上传失败', icon: 'none' })
+            }
+          } catch (e) {
+            uni.showToast({ title: '上传失败', icon: 'none' })
+          }
+        },
+        fail: () => {
+          uni.showToast({ title: '上传请求失败', icon: 'none' })
+        },
+        complete: () => {
+          uni.hideLoading()
+        }
+      })
+    },
+    deleteAvatar() {
+      this.avatarUrl = ''
+      uni.showToast({ title: '已删除头像', icon: 'success' })
+    },
+
     // 注册
     async handleRegister() {
       const { account, password, confirmPwd } = this.form
@@ -34,7 +88,10 @@ export default {
           method: 'POST',
           data: {
             username: account,
-            password: password
+            password: password,
+            avatar: this.avatarUrl || '',
+            nickname: this.form.nickName || '',
+            phone: this.form.phone || ''
           }
         })
 
--- pages/register/register.vue
+++ pages/register/register.vue
@@ -1,32 +1,56 @@
 <template>
   <view class="register-page">
-    <!-- 顶部标题 -->
-    <view class="header">
-      <view class="main-title">欢迎来到企业出海资讯平台</view>
-      <view class="sub-title">你的实时新闻助手</view>
-    </view>
-
-    <!-- 注册表单 -->
-    <view class="form-box">
-      <view class="input-item">
-        <input v-model="form.account" placeholder="请输入账号/手机号" type="text" />
+    <view class="register-container">
+      <!-- 顶部标题 -->
+      <view class="header">
+        <view class="main-title">欢迎来到企业出海资讯平台</view>
+        <view class="sub-title">你的实时新闻助手</view>
       </view>
 
-      <view class="input-item">
-        <input v-model="form.password" placeholder="请设置密码" password />
+      <!-- 头像上传区域 -->
+      <view class="avatar-upload" @click="chooseAvatar">
+        <!-- 已上传则显示头像，未上传则显示 + 号 -->
+        <image v-if="avatarUrl" :src="avatarUrl" class="avatar-img" mode="aspectFill"></image>
+        <view v-else class="avatar-placeholder">
+          <text class="plus">+</text>
+        </view>
+
+        <!-- 已上传头像 → 显示删除按钮 -->
+          <view v-if="avatarUrl" class="avatar-delete" @click.stop="deleteAvatar">
+            <text>×</text>
+          </view>
       </view>
 
-      <view class="input-item">
-        <input v-model="form.confirmPwd" placeholder="请确认密码" password />
-      </view>
+      <!-- 注册表单 -->
+      <view class="form-box">
+        <view class="input-item">
+          <input v-model="form.account" placeholder="请输入账号/手机号" type="text" />
+        </view>
 
-      <button class="register-btn" @click="handleRegister" :disabled="loading">
-        <text v-if="!loading">注册</text>
-        <text v-else>注册中...</text>
-      </button>
+        <view class="input-item">
+          <input v-model="form.password" placeholder="请设置密码" password />
+        </view>
 
-      <view class="link-row">
-        <view class="link" @click="goLogin">已有账号？返回登录</view>
+        <view class="input-item">
+          <input v-model="form.confirmPwd" placeholder="请确认密码" password />
+        </view>
+
+        <view class="input-item">
+          <input v-model="form.nickName" placeholder="昵称" />
+        </view>
+
+        <view class="input-item">
+          <input v-model="form.phone" placeholder="电话" />
+        </view>
+
+        <button class="register-btn" @click="handleRegister" :disabled="loading">
+          <text v-if="!loading">注册</text>
+          <text v-else>注册中...</text>
+        </button>
+
+        <view class="link-row">
+          <view class="link" @click="goLogin">已有账号？返回登录</view>
+        </view>
       </view>
     </view>
   </view>
